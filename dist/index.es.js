b8ec436319c32962b8ac68eab1791ae407b07672
Nick930826 <xianyou1993@qq.com>
2022-02-08 15:37:44 +0800

    update

--- dist/index.es.js
+++ dist/index.es.js
@@ -1,29 +1,24 @@
-import { defineComponent, openBlock, createElementBlock, renderSlot } from "vue";
-var index_vue_vue_type_style_index_0_scoped_true_lang = "";
-var _export_sfc = (sfc, props) => {
-  const target = sfc.__vccOpts || sfc;
-  for (const [key, val] of props) {
-    target[key] = val;
-  }
-  return target;
-};
+import { defineComponent, resolveComponent, openBlock, createBlock, withCtx, renderSlot } from "vue";
 const _sfc_main = /* @__PURE__ */ defineComponent({
   emits: ["click"],
   setup(__props) {
     return (_ctx, _cache) => {
-      return openBlock(), createElementBlock("button", {
+      const _component_el_button = resolveComponent("el-button");
+      return openBlock(), createBlock(_component_el_button, {
         class: "my-button",
         onClick: _cache[0] || (_cache[0] = ($event) => _ctx.$emit("click", $event))
-      }, [
-        renderSlot(_ctx.$slots, "default", {}, void 0, true)
-      ]);
+      }, {
+        default: withCtx(() => [
+          renderSlot(_ctx.$slots, "default")
+        ]),
+        _: 3
+      });
     };
   }
 });
-var Button = /* @__PURE__ */ _export_sfc(_sfc_main, [["__scopeId", "data-v-4a5dd954"]]);
 const ButtonPlugin = {
   install(app) {
-    app.component("my-button", Button);
+    app.component("my-button", _sfc_main);
   }
 };
 const MyKitPlugin = {
@@ -32,4 +27,4 @@ const MyKitPlugin = {
     (_a = ButtonPlugin.install) == null ? void 0 : _a.call(ButtonPlugin, app);
   }
 };
-export { Button, ButtonPlugin, MyKitPlugin as default };
+export { _sfc_main as Button, ButtonPlugin, MyKitPlugin as default };
